00c903ae1240ccd4f2132e4b36e5d0ec42f697ee
Fedor Ptitsyn <fedya.ptitsyn@mail.ru>
2023-01-16 11:09:12 +0300

    Add files via upload

--- 14_1.py
+++ 14_1.py
@@ -1,33 +1,15 @@
 def f(x, q):
-    s = 0
+    a = 0
     for i in range(-1, -len(x) - 1, -1):
-        s += x[i] * q ** (abs(i) - 1)
-
-    return s
-
-
-# ord
-# chr
-
-n = 10
-for i in range(ord("A"), ord("Z")+1):
-    print(n, chr(i))
-    n += 1
-
-ans = []
-for a in range(55):
-    a1 = [35, a, 34, 33]
-    a2 = [2, 33, a, 34]
-    if (f(a1, 55) - f(a2, 55)) % 29 == 0:
-        ans.append(a)
+        a += x[i] * q ** (abs(i) - 1)
+    return a
 
 
-a1 = [35, max(ans), 34, 33]
-a2 = [2, 33, max(ans), 34]
-a3 = [35, min(ans), 34, 33]
-a4 = [2, 33, min(ans), 34]
-
-b1 = f(a1, 55) - f(a2, 55)
-b2 = f(a3, 55) - f(a4, 55)
-
-print(abs(b1 - b2))
+for x in range(1, 8):
+    for y in range(8):
+        a1 = f([x, 0, 1, y, 4], 9)
+        a2 = f([x, y, 5, 4, 4], 8)
+        d = a1 + a2
+        if d % 89 == 0:
+            print(x, y)
+            print(d // 89)
--- 14_2.py
+++ 14_2.py
@@ -1,14 +1,14 @@
-def f(x, q):
-    s = 0
-    for i in range(-1, -len(x) - 1, -1):
-        s += x[i] * q ** (abs(i) - 1)
+def intt(s, t):
+    s = s[::-1]
+    a = 0
+    for i in range(len(s)):
+        a += int(s[i]) * int(t) ** i
+    return a
 
-    return s
 
-
-for x in range(15):
-    for y in range(17):
-        a1 = f([1, 2, 3, x, 5], 15)
-        a2 = f([6, 7, y, 9], 17)
-        if (a1 + a2) % 131 == 0:
-            print(x, y, (a1 + a2) // 131)
+for x in range(10):
+    a1 = intt("13", f"132{x}4")
+    a2 = int(f"134{x}2", 13)
+    d = abs(a1 - a2)
+    if d % 30 == 0:
+        print(x, d // 30)
